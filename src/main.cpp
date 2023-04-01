36af63cb164a3691de500e299e538e90b9589f8c
Robert-Dobson <43008203+Robert-Dobson@users.noreply.github.com>
2023-04-01 16:49:19 +0100

    added update_leds function
    Added function to update leds based on an array

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
+int led_pins[] = {2, 3, 4, 5, 6, 7};
+const int num_of_leds = 6;
+
+void update_leds(int led_vals[num_of_leds]){
+  for (int i = 0; i < num_of_leds; i++){
+    digitalWrite(led_pins[i], led_vals[i]);
+  }
+  delay(100);
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  // set LED pins as outputs
+  for (int i = 0; i < num_of_leds; i++){
+    pinMode(led_pins[i], OUTPUT);
+  }
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int light[] = {1, 1, 1, 1, 1, 1};
+  update_leds(light);
+  int dark[] = {0, 0, 0, 0, 0, 0};
+  update_leds(dark);
 }
+
