ec1b1ece6bb173dc3ae42f538813e742c8040978
SilverSaber62 <95030775+SilverSaber62@users.noreply.github.com>
2023-04-01 23:24:42 +0100

    Merged Changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,33 @@
 #include <Arduino.h>
 #include "text_frames.h"
+#include <time.h>
 
 // Constants
 const int led_pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 const int num_of_leds = 8;
 const int size_of_frame = 100;
+double angle = 0;
+double anglePerSecond = 6;
+clock_t lastUpdateTime;
+int sliceIndex = 0;
 
 // Screen
 unsigned char *frame = NULL;
 int size = 0;
 
-void update_leds(unsigned char led_vals){
-  for (int i = 0; i < num_of_leds; i++){
+// Pass in a slice
+void update_leds(unsigned char led_vals)
+{
+  for (int i = 0; i < num_of_leds; i++)
+  {
     int val = (led_vals >> i) & 0x01;
     digitalWrite(led_pins[i], val);
   }
 }
 
-void print_frame(unsigned char *frame, int size){
+// Testing only
+void print_frame(unsigned char *frame, int size)
+{
   // change to update with angle
   for (int i = 0; i < size; i++)
   {
@@ -25,26 +35,64 @@ void print_frame(unsigned char *frame, int size){
   }
 }
 
-void print_text(char *text){
-  TextFrame textFrame;
-  frame = textFrame.convertStringToFrame("Hello");
-  size = textFrame.getSize();
-  
-  print_frame(frame, size);
-  delay(1000000);
+// void print_text(char *text)
+// {
+//   TextFrame textFrame;
+//   char text[] = {'H', 'E', 'S', 'T'};
+//   frame = textFrame.convertStringToFrame("Hello");
+//   size = textFrame.getSize();
 
-  // Free 
-  free(frame);
-  frame = NULL;
-}
+//   print_frame(frame, size);
+//   delay(1000000);
 
-void setup() {
-  // set LED pins as outputs
-  for (int i = 0; i < num_of_leds; i++){
-    pinMode(led_pins[i], OUTPUT);
+//   // Free
+//   free(frame);
+//   frame = NULL;
+// }
+
+int getSliceIndex(double angle, double maxAngle, double minAngle)
+{
+  // Calculate proportion through frame using angle then return correct slice of frame
+  if (angle < maxAngle && angle > minAngle)
+  {
+    return round((angle - minAngle) / (maxAngle - minAngle) * size);
+  }
+  else
+  {
+    return -1;
   }
 }
 
-void loop() {
+void updateAngle()
+{
+  clock_t currentTime = clock();
+  double timeDifference = (currentTime - lastUpdateTime);
+  angle += (anglePerSecond * timeDifference / 1000);
+  while (angle > (2 * M_PI))
+  {
+    angle -= 2 * M_PI;
+  }
+  lastUpdateTime = currentTime;
+}
 
+void setup()
+{
+  // set LED pins as outputs
+  for (int i = 0; i < num_of_leds; i++)
+  {
+    pinMode(led_pins[i], OUTPUT);
+  }
+  TextFrame textFrame;
+  char text[] = {'T', 'E', 'S', 'T'};
+  frame = textFrame.convertStringToFrame(text);
+  size = textFrame.getSize();
+  lastUpdateTime = clock();
+}
+
+void loop()
+{
+  update_leds(frame[sliceIndex]);
+  // Delay
+  updateAngle();
+  sliceIndex = getSliceIndex(angle, (3 * M_PI) / 2, M_PI / 2);
 }
