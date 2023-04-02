671cbe5488034c85d2b303e6eb424d4db93ae5eb
Robert-Dobson <43008203+Robert-Dobson@users.noreply.github.com>
2023-04-02 10:01:21 +0100

    Setup demo script

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,12 @@ int index = 0;
 // Screen
 unsigned char *frame = NULL;
 int size = 0;
+unsigned char wave[9] = {0b00000000, 0b00000001,  0b00000011, 0b00000110,  0b00001100, 0b00011000,  0b00110000, 0b01100000, 0b11000000};
+bool increasing_wave = true;
 
 // Timing of demo
 unsigned long startTime = millis();
-int demoSelect = 0;
+int demoSelect = -1;
 const int numOfDemos = 6;
 char demoWords[numOfDemos][11] = {
   {' ', ' ', ' ', ' ', ' ', 'H', 'E', 'L', 'L', 'O', '\0'},
@@ -30,7 +32,7 @@ char demoWords[numOfDemos][11] = {
 };
 
 // Pass in a slice
-void update_leds(unsigned char led_vals)
+void update_leds(unsigned char led_vals, float time_delay = 1.6)
 {
   for (int i = 0; i < num_of_leds; i++)
   {
@@ -38,7 +40,7 @@ void update_leds(unsigned char led_vals)
     digitalWrite(led_pins[i], val);
     // Serial.print(val);
   }
-  delay(1.6);
+  delay(time_delay);
 }
 
 void updateTextFrame(char *text){
@@ -71,37 +73,86 @@ void updateTextFrame(char *text){
 
 void setup()
 {
-  Serial.begin(9600);
+  // Serial.begin(9600);
   // set LED pins as outputs
   pinMode(13, INPUT);
   for (int i = 0; i < num_of_leds; i++)
   {
     pinMode(led_pins[i], OUTPUT);
   }
+}
 
-  updateTextFrame(demoWords[0]);
+void textLoop(){
+  startTime = millis();
+  bool loop = true;
+  
+  while (loop){
+    unsigned long timeDif = millis() - startTime;
+    if (timeDif >= 7500 || demoSelect == -1){
+      demoSelect++;
+
+      if (demoSelect == numOfDemos){
+        demoSelect = -1;
+        loop = false;
+        break;
+      }
+      else{
+        updateTextFrame(demoWords[demoSelect]);
+        startTime = millis();
+      }
+    }
+
+    if (loop != false){
+      if (size != 0){
+        update_leds(frame[index]);
+        index++;
+        if (index == size){
+          index = 0;
+        }
+      }
+    }
+  }
+  
 }
 
 
 void loop()
 {
-  unsigned long timeDif = millis() - startTime;
-  if (timeDif >= 10000){
-    demoSelect++;
-
-    if (demoSelect == numOfDemos){
-      demoSelect = 0;
+  // Triangles and Squares
+  for (int n = 0; n < 300; n ++){
+    for (int i = 0; i < 8; i++){
+      update_leds(Triangle[i], 2);
     }
-
-    updateTextFrame(demoWords[demoSelect]);
-    startTime = millis();
+    update_leds(0, 2);
+    update_leds(0, 2);
+    for (int i = 0; i < 8; i++){
+      update_leds(Square[i], 2);
+    }
+    update_leds(0, 2);
+    update_leds(0, 2);
   }
+  
 
-  if (size != 0){
-    update_leds(frame[index]);
-    index++;
-    if (index == size){
-      index = 0;
+  // Text
+  textLoop();
+  
+  // Wave
+  for (int n = 0; n < 6; n += 1){
+    for (int i = 0; i < 9; i+=1){
+      update_leds(wave[i], n * 20);
+    }
+    for (int j = 8; j >= 0; j-=1){
+      update_leds(wave[j], n * 20);
     }
   }
+  
+  for (int n = 5; n >=0; n -= 1){
+    for (int i = 0; i < 9; i+=1){
+      update_leds(wave[i], n * 20);
+    }
+    for (int j = 8; j >= 0; j-=1){
+      update_leds(wave[j], n * 20);
+    }
+  }
+  
 }
